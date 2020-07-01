3dea13c77e6f626c41d47fd7122196b1f4b987cf
2017315012_YangDongJae <ydj9805@naver.com>
2020-07-01 13:34:25 +0900

    [master-03] Display the computer play
    [compare-01] Add the tie condition
    
    [compare-02] Add the rock case
    
    [compare-04] Add the scissors case

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -2,5 +2,29 @@
 play = ["Rock", "Paper", "Scissors"]
 # assign a random play to the computer
 computer = "Rock"
+print('Computer: {}'.format(computer))
 # get the user input
 player = "Paper"
+print('Player: {}'.format(player))
+# tie
+if player == computer:
+    print("Tie!")
+
+# rock
+elif player == "Rock":
+    if computer == "Scissors":
+        print("You win!")
+    else:
+        print("You lose!")
+# paper
+elif player == "Paper":
+    if computer == "Rock":
+        print("You win!")
+    else:
+        print("You lose!")
+# scissors
+elif player == "Scissors":
+     if computer == "Paper":
+         print("You win!")
+     else:
+         print("You lose!")
